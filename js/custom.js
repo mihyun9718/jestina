e485a2f64fe0aa2d2ae21e0667cce59fbeda99e3
mihyun9718 <algy9718@naver.com>
2024-10-04 15:02:48 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+
+//메인비주얼 슬라이드
+$(function () {
+    $('.main_visual_slide').slick({
+        arrows:  false,
+        dots: false,
+        autoplay: true,
+        pauseOnHover: false,
+        useTransform: false
+    });
+
+    //슬라이드로 탭구현.
+    $('.main_visual_slide').on('afterChange', function (e, s, c) {
+        $('.main_visual .lnk li').removeClass('on');
+        $('.main_visual .lnk li').eq(c).addClass('on')
+    });
+
+    //슬라이드 arrow
+    $('.main_visual .arrows .prev').on('click', function () {
+        $('.main_visual_slide').slick('slickPrev')
+    });
+
+    $('.main_visual .arrows .next').on('click', function () {
+        $('.main_visual_slide').slick('slickNext')
+    });
+
+});
